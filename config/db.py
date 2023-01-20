9604e035318a7627b8b0aa587e820bf347973985
adiazc50 <adiazc@eafit.edu.co>
2023-01-20 03:57:04 -0500

    implementacion del get/all get/especific put

--- config/db.py
+++ config/db.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine, MetaData
 
 engine= create_engine("mysql+pymysql://root:toor@localhost:3306/biblioteca")
 
-conn= engine.connect()
+
 
 meta_data= MetaData()
--- route/route.py
+++ route/route.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from starlette.status import HTTP_201_CREATED
 from schema.biblioteca_Schema import bibliotecaSchema
-from config.db import conn
+from config.db import engine
 from model.biblioteca import biblioteca
 from typing import List
 
@@ -10,16 +11,34 @@ bibliotec= APIRouter()
 def root():
     return{"message":"Bienvenidos al gestor de la API, para visualizar rutas dirigete a /docs"}
 
+@bibliotec.get("/api/libros",response_model=List[bibliotecaSchema])
+def mostrarLibros():
+    with engine.connect() as conn:
+        resultado = conn.execute(biblioteca.select()).fetchall()
+        return resultado
 
-@bibliotec.post("/api/crearLibros")
+@bibliotec.get("/api/consultarLibro/{isbnId}", response_model=bibliotecaSchema)
+def obtenerLibro(isbnId: str):
+    with engine.connect() as conn:
+        resultado= conn.execute(biblioteca.select().where(biblioteca.c.isbnId== isbnId)).first()
+        return resultado
+
+
+@bibliotec.post("/api/crearLibros", status_code=HTTP_201_CREATED)
 def crearLibros(data_libro: bibliotecaSchema):
-    nuevoLibro= data_libro.dict()
-    conn.execute(biblioteca.insert().values(nuevoLibro))
-    return "Success"
+    with engine.connect() as conn:
+        nuevoLibro= data_libro.dict()
+        conn.execute(biblioteca.insert().values(nuevoLibro))
+        return Response(status_code=HTTP_201_CREATED)
 
-@bibliotec.put("/api/actualizarEstado")
-def actualizarLibro():
-    pass
+@bibliotec.put("/api/actualizarEstado/{isbnId}")
+def actualizarLibro(data_update:bibliotecaSchema,isbnId: str):
+    with engine.connect() as conn:
+        conn.execute(biblioteca.update().values(titulo=data_update.titulo,autor=data_update.autor,
+        editorial=data_update.editorial,coleccion=data_update.coleccion,cantidad=data_update.cantidad,diponible=data_update.diponible).where(biblioteca.c.isbnId == isbnId))
+ 
+        resultado = conn.execute(biblioteca.select().where(biblioteca.c.isbnId == isbnId)).first()
+        return resultado
 
 
 
