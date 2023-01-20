62214838d2a7bda462fc7508f82b083b0d561271
adiazc50 <adiazc@eafit.edu.co>
2023-01-20 01:41:14 -0500

    apunto en DB local por error de encriptacion para conectar en Azure

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine= create_engine("mysql+pymysql://administrador:leonisaApi1@testleonisaapi.mysql.database.azure.com:3306/biblioteca")
+engine= create_engine("mysql+pymysql://root:toor@localhost:3306/biblioteca")
 
 conn= engine.connect()
 
