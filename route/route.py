4ed58b2d18281eadd9bed373233544051857484d
adiazc50 <adiazc@eafit.edu.co>
2023-01-19 19:16:15 -0500

    creacion de POST

--- route/route.py
+++ route/route.py
@@ -1,13 +1,22 @@
 from fastapi import APIRouter
 from schema.biblioteca_Schema import bibliotecaSchema
+#from config.db import conn
+#from model.biblioteca import bibliotecas
+
+
 biblioteca= APIRouter()
 
 @biblioteca.get("/")
-def root(title='Test Leonisa',
-            description='para visualizar la guia de la apli /routes',
-            version='1'):
-    return{"message":"Bienvenidos al gestor de la API"}
+def root():
+    return{"message":"Bienvenidos al gestor de la API, para visualizar rutas dirigete a /docs"}
 
 @biblioteca.post("/api/agregarLibro")
 def crearLibro(data_Libro:bibliotecaSchema):
-    print(data_Libro)
+    nuevo_libro = data_Libro.dict()
+    #conn.execute(bibliotecas.insert().values(nuevo_libro)) #->datalibro
+    #print(data_Libro)
+    #print(nuevo_libro)
+
+@biblioteca.put("/api/actualizarEstado")
+def actualizarLibro():
+    pass
