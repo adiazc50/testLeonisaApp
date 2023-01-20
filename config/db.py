c7d029dfeeae4f4cbb53d2f90fa61566133592d2
adiazc50 <adiazc@eafit.edu.co>
2023-01-19 20:27:10 -0500

    creacion de GET* get

--- config/db.py
+++ config/db.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine, MetaData
 
 engine= create_engine("mysql+pymysql://root@localhost:3306/nombredb")
 
-conn = engine.connect()
+
 
 meta_data= MetaData()
--- route/route.py
+++ route/route.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from schema.biblioteca_Schema import bibliotecaSchema
-#from config.db import conn
+from config.db import engine
 #from model.biblioteca import bibliotecas
-
+#from typing import List
 
 biblioteca= APIRouter()
 
@@ -10,13 +10,31 @@ biblioteca= APIRouter()
 def root():
     return{"message":"Bienvenidos al gestor de la API, para visualizar rutas dirigete a /docs"}
 
+"""""
+@biblioteca.get("api/ObtenerLibros", response_model=List[bibliotecaSchema])
+def obtenerLibros():
+    #with engine.connect() as conn:
+        #resultado= conn.execute(bibliotecas.select().fetchall())
+        #return resultado
+
+
+@biblioteca.get("api/libro/{isbnId_Id}", response_model=bibliotecaSchema)
+def buscarLibro(isbnId_Id: str):
+    #with engine.connect() as conn:
+        #resultado= conn.execute(bibliotecas.select().where(bibliotecas.c.isbnId==isbnId_Id)).first()
+        #return resultado
+
 @biblioteca.post("/api/agregarLibro")
 def crearLibro(data_Libro:bibliotecaSchema):
-    nuevo_libro = data_Libro.dict()
-    #conn.execute(bibliotecas.insert().values(nuevo_libro)) #->datalibro
-    #print(data_Libro)
-    #print(nuevo_libro)
+    print(data_Libro)
+    #with engine.connect() as conn:
+        #nuevo_libro = data_Libro.dict()
+        #conn.execute(bibliotecas.insert().values(nuevo_libro)) #->datalibro
+    
+        #print(nuevo_libro)
+    
 
 @biblioteca.put("/api/actualizarEstado")
 def actualizarLibro():
     pass
+"""
